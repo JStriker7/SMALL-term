f06c9c38124771c5830dd2ef9d2a2f38c24317df
JStriker7 <yuxinsun@163.com>
2025-07-10 09:21:50 +0800

    重构后

--- 持股情况表格.cpp
+++ 持股情况表格.cpp
@@ -1,105 +1,89 @@
 #include <iostream>
+#include <iomanip>
 #include "class.h"
-#include <vector>
-#include <iomanip> 
-#include <cmath>
+
 using namespace std;
 
-int main()
-{
-	char type;
-	string id;
-	string cur_type;
-	int num;
-	int value;
-	string ori_cur, cur;
-	double r;
-	vector<stock> account;
-	vector<rate> rates;
-	int a, b, c;
-	cin >> a;
-	while (a--)
-	{
-		cin >> type >> id >> cur_type >> num >> value;
-		account.emplace_back(type, id, cur_type, num, value);
-	}
-	cin >> b;
-	while (b--)
-	{
-		cin >> ori_cur >> cur >> r;
-		rates.emplace_back(ori_cur, cur, r);
+int main() {
+    // 创建市场
+    Market market;
 
-	}
-	cin >> c;
-	string q_type;
-	double result = 0;
-	while (c--)
-	{
-		cin >> q_type;
-		if (q_type == "PERSON")
-		{
-			string id, cur_type;
-			cin >> id >> cur_type;
-			for (int i = 0; i < account.size(); i++)
-			{
-				if (account[i].name() == id)
-				{
-					if (cur_type == account[i].currency())
-					{
-						 result = floor(account[i].self() * 100) / 100.0;
-						
-					}
-					else
-					{
-						//先计算总金额，再利用利率转换为刚刚输入的cur——type类型的总金额
-						double a = account[i].self();
-						//关键点在怎么找到对应利率
-						for (int j = 0; j < rates.size(); j++)
-						{
-							if (rates[j].o() == account[i].currency() && rates[j].c() == cur_type)
-							{
-								double re = a * rates[j].ra();
-								 result= floor(re * 100) / 100.0;
-								
-							}
-						}
-					}
-				}
-				
-			}
-		}
-		else if(q_type=="STOCK")//如何计算total//遍历account，如果有不是char类型的股，不加入计算，如果有不是type类型的金额的，先转换成对应类型的币种
-		{
-			char a;
-			string type;
-			cin >> a >>type;
-			for (int i = 0; i < account.size(); i++)
-			{
-				if (account[i].st() == a)
-				{
-					if (account[i].currency() == type)
-					{
-						stock::total += account[i].self();
+    // 读取股票数据
+    int stock_count;
+    cin >> stock_count;
 
-					}
-					else
-					{
-						for (int j = 0; j < rates.size(); j++)
-						{
-							if (rates[j].o() == type && rates[j].c() == account[i].currency())
-							{
-								double re = account[i].self() / rates[j].ra();
-								stock::total += re;
-								
-							}
-							
-						}
-					}
-				}
-			}
+    map<string, unique_ptr<User>> user_map;
 
-		}
-	}
-	cout << fixed << setprecision(2) << result << endl;
-	cout << fixed << setprecision(2) << stock::total << endl;
+    for (int i = 0; i < stock_count; i++) {
+        char type;
+        string id, currency;
+        int num;
+        int value;
+        cin >> type >> id >> currency >> num >> value;
+
+        // 创建股票 - 使用原来的stock类(现在继承自Asset)
+        auto s = make_shared<stock>(type, id, currency, num, value);
+
+        // 查找或创建用户
+        if (user_map.find(id) == user_map.end()) {
+            user_map[id] = make_unique<User>(id, "User_" + id);
+        }
+
+        // 添加股票到用户投资组合
+        user_map[id]->addStock(s);
+    }
+
+    // 将所有用户添加到市场
+    for (auto& pair : user_map) {
+        market.addUser(move(pair.second));
+    }
+
+    // 读取汇率数据
+    int rate_count;
+    cin >> rate_count;
+
+    Conv& conv = market.getConv();
+    for (int i = 0; i < rate_count; i++) {
+        string ori_cur, cur;
+        double rate_value;
+        cin >> ori_cur >> cur >> rate_value;
+        conv.add(rate(ori_cur, cur, rate_value));
+    }
+
+    // 处理查询
+    int query_count;
+    cin >> query_count;
+
+    Proc proc(market);
+    double person_result = 0.0;
+    double stock_result = 0.0;
+
+    for (int i = 0; i < query_count; i++) {
+        string query_type;
+        cin >> query_type;
+
+        if (query_type == "PERSON") {
+            string person_id, currency;
+            cin >> person_id >> currency;
+
+            // 使用多态方式处理查询
+            auto query = make_unique<PQuery>(market, person_id, currency);
+            person_result = proc.run(move(query));
+        }
+        else if (query_type == "STOCK") {
+            char stock_type;
+            string currency;
+            cin >> stock_type >> currency;
+
+            // 使用多态方式处理查询
+            auto query = make_unique<SQuery>(market, stock_type, currency);
+            stock_result = proc.run(move(query));
+        }
+    }
+
+    // 输出结果
+    cout << fixed << setprecision(2) << person_result << endl;
+    cout << fixed << setprecision(0)<< stock_result << endl;
+
+    return 0;
 }
